ccd596e2f8226995db952a1672b6ae67717bf166
Olekwer <olekwer@gmail.com>
2016-08-12 18:55:38 +0300

    Update addBook.js

--- TestAngular/public/js/directives/addBook.js
+++ TestAngular/public/js/directives/addBook.js
@@ -10,15 +10,19 @@ app.directive('addBook', function(){
         },
         restrict: 'E',
         templateUrl: 'view/pages/add-book-directive.html',
-        controller: function($scope, Qury){
+        controller: function($scope,$location,$timeout, Qury){
             $scope.add=function(name, author, image){
                 console.log(name);
                 console.log(author);
                 image="../img/image.jpg"
                 console.log(image);
                 Qury.add(name, author, image).success(function(data){
+                    console.log($location.path("/"));
 
                 });
+                $timeout(function(){
+                    $location.path('/');
+                },1000)
             }
         }
     };
